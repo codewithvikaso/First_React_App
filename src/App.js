7b24794d099b884e8775ae720963991ead6dd573
vikas <vikas>
2023-06-14 15:40:10 +0530

    Added two new lines

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
 
       <div>
         <h1>Hello, This is Vikas</h1>
+        <h1>I am a Python Developer</h1>
+        <p>Also, I am a Django Developer</p>
       </div>
 
     </>
